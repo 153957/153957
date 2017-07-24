3c7ee4ae61bd48d295a1989e298313dc3c1db299
Arne de Laat <arne@delaat.net>
2017-07-24 22:39:35 +0200

    Update to latest postcss-cssnext. This includes the newer postcss-nesting. Remove the separate postcss-nesting step. Clearer commands using more verbose flags.

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = (context) => ({
     plugins: {
         'postcss-each': {},
         'stylelint': context.env === 'development' ? {} : false,
-        'postcss-nesting': context.env === 'development' ? {} : false,
         'postcss-cssnext': context.env === 'development' ? {} : false
     }
 })
