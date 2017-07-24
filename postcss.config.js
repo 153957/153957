13ef2ad6912564d17cd4a930e5ce765f5330eb99
Arne de Laat <arne@delaat.net>
2017-07-24 23:21:15 +0200

    Improve postcss config logic.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = (context) => ({
     plugins: {
         'postcss-each': {},
-        'stylelint': context.env === 'development' ? {} : false,
-        'postcss-cssnext': context.env === 'development' ? {} : false
+        'stylelint': context.env === 'test' ? false : {},
+        'postcss-cssnext': context.env === 'test' ? false : {}
     }
 })
