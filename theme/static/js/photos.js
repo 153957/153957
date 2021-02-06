da2673d1c18beba53b6d681c4409e383968e5b0d
Arne de Laat <arne@delaat.net>
2021-02-06 17:35:20 +0100

    Add automatic iframe resizer for photo pages

--- theme/static/js/photos.js
+++ theme/static/js/photos.js
@@ -3,4 +3,8 @@
 $(document).ready(function() {
     var target = $("#target");
     target.prop("src", target.data("album") + window.location.hash.slice(1));
+    target.iFrameResize({
+        minHeight: 250,
+        maxHeight: 2000,
+    });
 });
