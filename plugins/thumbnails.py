541e46e7049d6300c0c495aac51bb72845362fbc
Arne de Laat <arne@delaat.net>
2021-03-13 14:36:45 +0100

    Use multiprocessing for thumbnail creation

--- plugins/thumbnails.py
+++ plugins/thumbnails.py
@@ -9,11 +9,23 @@ images will be resized by 50% and stored next to the original in the
 same format, but without the `@2x` suffix.
 
 """
+from multiprocessing import Pool
+
 from PIL import Image
 
 from pelican import signals
 
 SETTINGS_NAME = 'THUMBNAIL_PATHS'
+
+
+def create_thumbnail(image_path):
+    extension = image_path.suffix
+    image = Image.open(image_path)
+    image.thumbnail((
+        image.size[0] / 2,
+        image.size[1] / 2
+    ))
+    image.save(str(image_path).replace(f'@2x{extension}', extension))
 
 
 def create_thumbnails(instance):
@@ -23,15 +35,15 @@ def create_thumbnails(instance):
     extensions = ['.png', '.jpg', '.jpeg']
     paths = instance.settings.get(SETTINGS_NAME, [])
 
-    for extension in extensions:
-        for path in paths:
-            for image_path in path.rglob(f'*@2x{extension}'):
-                image = Image.open(image_path)
-                image.thumbnail((
-                    image.size[0] / 2,
-                    image.size[1] / 2
-                ))
-                image.save(str(image_path).replace(f'@2x{extension}', extension))
+    image_paths = [
+        image_path
+        for extension in extensions
+        for path in paths
+        for image_path in path.rglob(f'*@2x{extension}')
+    ]
+
+    with Pool() as pool:
+        pool.map(create_thumbnail, image_paths)
 
 
 def register():
